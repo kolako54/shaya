5552f795fe1f5e4907109c6656cc58fc61844e52
Annatar <itsAnnatar@gmail.com>
2024-02-19 13:21:31 +0330

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
 import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Signup } from "./pages/Signup";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </>
+  );
 };
 
 export default App;
