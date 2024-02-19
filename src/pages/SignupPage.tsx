9e739ffaf40c40a9891afd77396660d133f2908d
Annatar <itsAnnatar@gmail.com>
2024-02-19 21:04:52 +0330

    fix: make the nav and signup page fullscreen

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -2,7 +2,7 @@ import { Signup } from "@/components/Signup";
 
 export const SignupPage = () => {
   return (
-    <div className="mx-auto sm:w-3/5 ">
+    <div className="mx-auto w-3/4 sm:w-3/5 ">
       <div className="mb-8 flex items-center justify-between gap-4 pt-12">
         <div className="relative h-[1rem] flex-1 border-b-2 border-[#33363A]">
           <div className="absolute right-[-1px] top-2 size-3 rounded-full bg-[#33363A]" />
