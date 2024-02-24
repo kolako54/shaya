7a2456bbe6b83166efb0b2c7860c7abf11e6adea
Annatar <itsAnnatar@gmail.com>
2024-02-24 19:04:59 +0330

    feat: add main content to homepage

---
+++ src/components/MainHome.tsx
@@ -0,0 +1,134 @@
+export const MainHome = () => {
+  return (
+    <>
+      <div className="container  my-16">
+        <p className="relative top-[2.1rem] mb-4 text-center text-3xl font-bold text-white">
+          پاترئون چیست
+        </p>
+        <div className="custom-border relative text-center">
+          <div className="flex justify-between">
+            <div className=" relative bottom-[.75rem] h-[1rem] w-1/6 border-b-4 border-[#33363A] sm:w-1/4 md:w-1/3">
+              <div className="absolute right-[-1px] top-2 size-3 rounded-full bg-outlineColor" />
+            </div>
+            <div className=" relative  bottom-[.75rem] h-[1rem] w-1/6 border-b-4 border-[#33363A] sm:w-1/4 md:w-1/3">
+              <div className="absolute left-0 top-2 size-3 rounded-full bg-outlineColor" />
+            </div>
+          </div>
+          <p className="line-h-2 d-rtl p-6 text-start text-textMuted md:p-8">
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ. و با
+            استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است. و برای شرایط فعلی تکنولوژی مورد نیاز,
+            و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد, کتابهای
+            زیادی در شصت و سه درصد گذشته حال و آینده. شناخت فراوان جامعه و
+            متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان
+            رایانه ای علی الخصوص طراحان خلاقی؛ و فرهنگ پیشرو در
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="grid gap-y-14 sm:gap-y-10">
+          <div className="grid place-items-center items-center justify-between sm:flex">
+            <img
+              className="relative size-[10rem] opacity-40 hover:opacity-100 hover:transition-all hover:duration-500 md:size-[20rem] lg:size-[30rem]"
+              src="/XzA1Mzg2MDguanBn.png"
+              alt="people1"
+            />
+            <div className="relative bottom-[.75rem] left-[-1rem] hidden h-[1rem] w-1/4 border-b-4 border-[#33363A]  sm:block md:left-[-2rem] md:w-1/3 ">
+              <div className="absolute right-[-1px] top-2 size-3  rounded-full bg-outlineColor" />
+            </div>
+            <div className="grid gap-2">
+              <p className="d-rtl text-center text-lg text-white sm:text-start xl:text-2xl">
+                یک جریان درآمد مکرر ایجاد کنید!
+              </p>
+              <div className="dashed-card p-2 text-textMuted md:p-4">
+                <p className="d-rtl mb-4 md:text-lg lg:text-xl ">
+                  درآمد قابل اعتماد ماهانه اين امکان را برای من ایجاد کرده است
+                  که می توانم زمان کمتری را برای تعقیب چک و زمان بیشتری را برای
+                  خلق کردن صرف کنم
+                </p>
+                <span className="text-sm md:text-lg">
+                  سمیرا رازقی - نویسنده
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="grid flex-row-reverse place-items-center items-center justify-between sm:flex">
+            <img
+              className="relative size-[10rem] opacity-40 hover:opacity-100 hover:transition-all hover:duration-500 md:size-[20rem] lg:size-[30rem]"
+              src="/XzA4MDY3MTQuanBn.png"
+              alt="people2"
+            />
+            <div className=" relative bottom-[.75rem] right-[-1rem] hidden h-[1rem] w-1/4 border-b-4  border-[#33363A] sm:block md:right-[-2rem] md:w-1/3 ">
+              <div className="absolute left-[-1px] top-2 size-3  rounded-full bg-outlineColor" />
+            </div>
+            <div className="grid gap-2">
+              <p className="d-rtl text-center text-lg text-white sm:text-start xl:text-2xl">
+                یک جریان درآمد مکرر ایجاد کنید!
+              </p>
+              <div className="dashed-card p-2 text-textMuted md:p-4">
+                <p className="d-rtl mb-4  md:text-lg lg:text-xl ">
+                  درآمد قابل اعتماد ماهانه اين امکان را برای من ایجاد کرده است
+                  که می توانم زمان کمتری را برای تعقیب چک و زمان بیشتری را برای
+                  خلق کردن صرف کنم
+                </p>
+                <span className="text-sm md:text-lg">
+                  سمیرا رازقی - نویسنده
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="grid place-items-center items-center justify-between sm:flex">
+            <img
+              className="relative size-[10rem] opacity-40 hover:opacity-100 hover:transition-all hover:duration-500 md:size-[20rem] lg:size-[30rem]"
+              src="/LmpwZw.png"
+              alt="people3"
+            />
+            <div className="relative bottom-[.75rem] left-[-1rem] hidden h-[1rem] w-1/4 border-b-4 border-[#33363A]  sm:block md:left-[-2rem] md:w-1/3 ">
+              <div className="absolute right-[-1px] top-2 size-3  rounded-full bg-outlineColor" />
+            </div>
+            <div className="grid gap-2">
+              <p className="d-rtl text-center text-lg text-white sm:text-start xl:text-2xl">
+                یک جریان درآمد مکرر ایجاد کنید!
+              </p>
+              <div className="dashed-card p-2 text-textMuted md:p-4">
+                <p className="d-rtl mb-4  md:text-lg lg:text-xl ">
+                  درآمد قابل اعتماد ماهانه اين امکان را برای من ایجاد کرده است
+                  که می توانم زمان کمتری را برای تعقیب چک و زمان بیشتری را برای
+                  خلق کردن صرف کنم
+                </p>
+                <span className="text-sm md:text-lg">
+                  سمیرا رازقی - نویسنده
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="grid flex-row-reverse place-items-center items-center justify-between sm:flex">
+            <img
+              className="relative size-[10rem] opacity-40 hover:opacity-100 hover:transition-all hover:duration-500 md:size-[20rem] lg:size-[30rem]"
+              src="/LmpwZw (1).png"
+              alt="people4"
+            />
+            <div className=" relative bottom-[.75rem] right-[-1rem] hidden h-[1rem]  w-1/4 border-b-4 border-[#33363A] sm:block md:right-[-2rem] md:w-1/3 ">
+              <div className="absolute left-[-1px] top-2 size-3  rounded-full bg-outlineColor" />
+            </div>
+            <div className="grid gap-2">
+              <p className="d-rtl text-center text-lg text-white sm:text-start xl:text-2xl">
+                یک جریان درآمد مکرر ایجاد کنید!
+              </p>
+              <div className="dashed-card p-2 text-textMuted md:p-4">
+                <p className="d-rtl mb-4  md:text-lg lg:text-xl ">
+                  درآمد قابل اعتماد ماهانه اين امکان را برای من ایجاد کرده است
+                  که می توانم زمان کمتری را برای تعقیب چک و زمان بیشتری را برای
+                  خلق کردن صرف کنم
+                </p>
+                <span className="text-sm md:text-lg">
+                  سمیرا رازقی - نویسنده
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
