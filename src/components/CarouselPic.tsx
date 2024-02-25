f0f83acb4d1a5bc5b0647fde9e25b664cf48c4c6
Elio <dev.with.elio@gmail.com>
2024-02-25 04:25:37 +0330

    feat: add carousel resoponsive

--- src/components/CarouselPic.tsx
+++ src/components/CarouselPic.tsx
@@ -1,3 +1,5 @@
+import Autoplay from "embla-carousel-autoplay";
+
 import {
   Carousel,
   CarouselContent,
@@ -8,24 +10,50 @@ import {
 
 const CarouselPic = () => {
   const imgs = [
-    "/carousel/guitar.png",
-    "/carousel/tailoring.png",
+    "/carousel/guitar.jpg",
+    "/carousel/tailoring.jpg",
     "/carousel/painter.png",
   ];
   console.log(imgs);
 
   return (
-    <div className="flex w-full items-center justify-center">
-      <Carousel className="w-[45%] border-4 border-bgSecondary">
+    <div className=" mt-10 flex w-full flex-col items-center gap-7 px-5 text-textMuted">
+      <p className="d-rtl text-center text-2xl font-bold text-white">
+        چه کسانی از پاترئون استفاده کنن؟
+      </p>
+      <p className="d-rtl text-center text-xl leading-10">
+        اگر میخای بدونی این سایت برای کیا مناسبه فقط کافیه به آیکون های حاشیه ی
+        <br />
+        سایت نگاه کنی.
+        <br />
+        درسته! فرقی نمیکنه از چه صنفی وارد پاترئون میشی یا شغل و کسب و کارت چیه،
+        تو
+        <br />
+        میتونی با شغلی که داری، کاربرهارو جذب کنی و بهشون چیزی یاد بدی و درآمد
+        کسب کنی. به همین راحتی!
+      </p>
+      <Carousel
+        className="w-2/3 overflow-hidden border-[10px] border-bgSecondary shadow-2xl lg:w-[50rem]"
+        opts={{
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 5000,
+          }),
+        ]}
+      >
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
+          {imgs.map((item, index) => (
             <CarouselItem key={index}>
-              <div className="p-1 shadow-2xl">
-                {/* <div> */}
-                <div className="flex aspect-[2/1] items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
+              <div className="shadow-2xl">
+                <div className="flex aspect-[2/1] items-center justify-center">
+                  <img
+                    src={item}
+                    alt={item.split("/")[2].split(".")[0]}
+                    className="bg-cover"
+                  />
                 </div>
-                {/* </div> */}
               </div>
             </CarouselItem>
           ))}
@@ -33,6 +61,15 @@ const CarouselPic = () => {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
+      <p className="d-rtl mt-10 text-center text-2xl font-bold text-white">
+        آسون تر از چیزیه که فکرشو میکنی
+      </p>
+      <p className="d-rtl text-center text-xl leading-10">
+        روش های مختلفی برای امتیاز گرفتن از طرفدارات وجود داره <br />
+        تو میتونی امتیاز خودتو همونطور که میخای دریافت کنی
+        <br />
+        مثال زیر رو مثلا برای یک گیتاریست میزنیم:
+      </p>
     </div>
   );
 };
