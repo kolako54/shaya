b2a1d772e9520c958a06cd047127869a23a947aa
Annatar <itsAnnatar@gmail.com>
2024-04-02 18:55:08 +0330

    refactor: modify routing and creator page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Collection } from "./features/Creator-dashboard/components/Collection";
 import { Home } from "./features/Creator-dashboard/components/Home";
 import { CreatorPage } from "./features/Creator-dashboard/containers/CreatorPage";
 import { Login } from "./features/Form/components/Login";
 import { Signup } from "./features/Form/components/Signup";
 import { AuthLayout } from "./features/Form/containers/AuthLayout";
 import { HomePage } from "./features/landing-page/containers/HomePage";
-import { Navbar } from "./features/Navbar/components/Navbar";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route element={<AuthLayout />}>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/creator" element={<CreatorPage />}>
-          <Route path="/creator" element={<Home />} />
-          <Route path="/creator/home" element={<Home />} />
-          <Route path="/creator/collection" element={<Collection />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+      <Route element={<CreatorPage />}>
+        <Route path="/creator" element={<Home />} />
+      </Route>
+    </Routes>
   );
 };
 
--- src/features/Navbar/components/Navbar.tsx
+++ src/features/Navbar/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import {
   NavigationMenu,
@@ -11,116 +11,110 @@ import {
 import { MobileNav } from "./MobileNav";
 
 export const Navbar = () => {
-  const location = useLocation();
-
   return (
-    <div>
-      {location.pathname === "/" && (
-        <div className="sticky top-0 z-[999] bg-secondary-color py-5 ">
-          <header className=" container flex items-center justify-between">
-            <img
-              src="/icons/main logo.png"
-              alt="header-img"
-              className="w-[2rem] cursor-pointer"
-            />
-            <nav className=" hidden flex-row-reverse gap-10 text-primary-text  transition-all hover:*:text-white hover:*:ease-out sm:flex">
-              <NavigationMenu>
-                <NavigationMenuList>
-                  <NavigationMenuItem>
-                    <NavigationMenuTrigger>سازندگان</NavigationMenuTrigger>
-                    <NavigationMenuContent className="flex flex-col gap-4 bg-card-color text-xs text-primary-text  hover:*:brightness-0  hover:*:invert hover:*:transition hover:*:ease-out">
-                      <div className="flex items-center justify-between ">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">پادکسترها</NavLink>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">سازندگان ویدئو</NavLink>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">موزیسین ها</NavLink>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">نقاشی دیجیتال</NavLink>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">جوامع</NavLink>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">نویسندگان</NavLink>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">سازندهای بازی</NavLink>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">موسسات خیریه</NavLink>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">آموزشی ها</NavLink>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <img
-                          className="size-8"
-                          src="/icons/web-development.png"
-                          alt=""
-                        />
-                        <NavLink to="/">کارهای محلی</NavLink>
-                      </div>
-                    </NavigationMenuContent>
-                  </NavigationMenuItem>
-                </NavigationMenuList>
-              </NavigationMenu>
-              <NavLink to="/">منابع</NavLink>
-              <NavLink to="/">قیمت گزاری</NavLink>
-              <NavLink to="/">محصول</NavLink>
-            </nav>
-            <MobileNav />
-          </header>
-        </div>
-      )}
+    <div className="sticky top-0 z-[999] bg-secondary-color py-5 ">
+      <header className=" container flex items-center justify-between">
+        <img
+          src="/icons/main logo.png"
+          alt="header-img"
+          className="w-[2rem] cursor-pointer"
+        />
+        <nav className=" hidden flex-row-reverse gap-10 text-primary-text  transition-all hover:*:text-white hover:*:ease-out sm:flex">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>سازندگان</NavigationMenuTrigger>
+                <NavigationMenuContent className="flex flex-col gap-4 bg-card-color text-xs text-primary-text  hover:*:brightness-0  hover:*:invert hover:*:transition hover:*:ease-out">
+                  <div className="flex items-center justify-between ">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">پادکسترها</NavLink>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">سازندگان ویدئو</NavLink>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">موزیسین ها</NavLink>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">نقاشی دیجیتال</NavLink>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">جوامع</NavLink>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">نویسندگان</NavLink>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">سازندهای بازی</NavLink>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">موسسات خیریه</NavLink>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">آموزشی ها</NavLink>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <img
+                      className="size-8"
+                      src="/icons/web-development.png"
+                      alt=""
+                    />
+                    <NavLink to="/">کارهای محلی</NavLink>
+                  </div>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <NavLink to="/">منابع</NavLink>
+          <NavLink to="/">قیمت گزاری</NavLink>
+          <NavLink to="/">محصول</NavLink>
+        </nav>
+        <MobileNav />
+      </header>
     </div>
   );
 };
